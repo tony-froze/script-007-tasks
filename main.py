3ab23249f04777286d30bdbea76d93b6fb5cf3d0
tony-froze <tony_froze@mail.ru>
2022-06-01 09:21:22 +0300

    Add argpasrse for main.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-def main():
-    pass
+import argparse
+
+
+def main(args: argparse.Namespace):
+    return args.directory
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", required=True, help="Working directory")
+    arguments = parser.parse_args()
+    main(arguments)

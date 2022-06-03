db7ac12ae00fdaec8c0051e354c9f549fe7a56df
Maxim Suslov <suslov.mn@gmail.com>
2022-06-03 09:42:03 +0300

    Add 05_rest_api

---
+++ demo/json/json_1_load.py
@@ -0,0 +1,14 @@
+import json
+
+text = """
+{
+  "id": 1000,
+  "request_processed": true,
+  "who": "myuser"
+}
+"""
+data = json.loads(text)
+print('type:', type(data))
+print('data:', data)
+print('who:', data.get('who', 'default'))
+print('who:', data['who'])
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import logging
 
 from dotmap import DotMap
+from aiohttp import web
+
+from server.WebHandler import WebHandler as wh
 
 from config import config
 import server.FileService as fs
@@ -11,6 +14,13 @@ from utils.log_utils import create_logs
 def main(args: DotMap):
     logging.debug('Start working...')
     logging.debug(f'Ready to listen to port {config.port}.')
+    handler = WebHandler()
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handler.handle),
+        # TODO: add more routes
+    ])
+    web.run_app(app, port=config.port)
     fs.change_dir(args.directory)
     fs.create_file('test.txt', '123')
     content = str(fs.get_files()[0].get('content', 'sample data'))
